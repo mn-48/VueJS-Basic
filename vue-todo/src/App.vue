a915f111df637fb8a353f1029330704781461bde
mn-48 <nazmul.cse48@gmail.com>
2025-09-22 09:58:48 +0600

    vue-todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <form @submit.prevent="addTodo">
+      <input type="text" placeholder="Add a todo" v-model="newTodo" />
+      <button type="submit">Add</button>
+    </form>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    // Emit the new todo item
+    // emit('add-todo', newTodo.value)
+    console.log('New Todo:', newTodo.value)
+    newTodo.value = ''
+  }
+}
+
+</script>
