eff6319650219a349c475c1e662e3f65c00b9838
mn-48 <nazmul.cse48@gmail.com>
2025-09-22 11:33:45 +0600

    OK

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,26 +1,67 @@
 <template>
   <div>
-
     <form @submit.prevent="addTodo">
-      <input type="text" placeholder="Add a todo" v-model="newTodo" />
+      <input type="text" placeholder="Add a description" v-model="data.todoDescriptions" />
       <button type="submit">Add</button>
+
     </form>
+    <ol>
+
+      <li v-for="(todo, index) in data.todos" :key="index">
+        <del v-if="todo.completed">{{ todo.description }}</del>
+        <span v-else>{{ todo.description }}</span>
+        <button @click="deleteTodo(index)">
+          Delete 
+          <i class="trash icon"> 🗑️</i>
+        </button>
+        <button @click="completeTodo(index) " :hidden="todo.completed">
+          Complete 
+          <i class="check icon"> ✅</i>
+        </button>
+        <!-- <button @click="completeTodo(index) " :disabled="todo.completed">
+          Complete 
+          <i class="check icon"> ✅</i>
+        </button> -->
+      </li>
+    </ol>
+
   </div>
 
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
-const newTodo = ref('')
+const data = reactive({
+  todos: [],
+  todoDescriptions: ""
+})  
+
+
+
+
 
 const addTodo = () => {
-  if (newTodo.value.trim()) {
-    // Emit the new todo item
-    // emit('add-todo', newTodo.value)
-    console.log('New Todo:', newTodo.value)
-    newTodo.value = ''
+  if (data.todoDescriptions.trim()) {
+
+    console.log('New Todo:', data.todoDescriptions);
+    data.todos.push({
+      description: data.todoDescriptions,
+      completed: false
+    });
+    data.todoDescriptions = '';
   }
 }
+
+const deleteTodo = (index) => {
+  data.todos.splice(index, 1);
+}
+
+const completeTodo = (index) => {
+  data.todos[index].completed = true;
+} 
+
+
+
 
 </script>
